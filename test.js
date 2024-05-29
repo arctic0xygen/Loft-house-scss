7e9faf9590eddde21c6f6d0fa928d57cde6fdfdc
arctic0xygen <ann.moraru@gmail.com>
2024-05-30 00:13:47 +0200

    ok let's gooo

---
+++ test.js
@@ -0,0 +1,23 @@
+// const numbers = [34, 26, 356, 12, 9, 5, 10]
+
+// numbers.sort((a, b) => a - b);
+
+// console.log(numbers);
+
+
+// function strCount(str, letter) {
+//     let newStr = str.split("");
+//     let veryNewStr = newStr.filter(elem => elem == letter);
+//     console.log(veryNewStr.length); 
+// }
+
+// strCount('bibik', 'b')
+
+
+
+function hzz (str) {
+    const blumblum = [...str]; // const blumblum = str.split("");
+    console.log(blumblum);
+}
+
+hzz('clopotel')
